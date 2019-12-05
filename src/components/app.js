a6b9d2a93d4f37eb99c730680ae49a55dd4e9861
Waddah Mortada <w.mortada87@gmail.com>
2019-12-05 20:20:36 +0000

    Fix loading map via home

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import Home from './home'
+
 class app extends Component {
   render () {
     return (
--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Row, Col, Container, Navbar } from 'react-bootstrap'
-import  Map from './map'
+import  Map from './Map'
 
 export default function home () {
   return (
