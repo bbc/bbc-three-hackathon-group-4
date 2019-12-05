21a8a2a5a142ae2dfed53c6f27d5cf753793121b
Waddah Mortada <w.mortada87@gmail.com>
2019-12-05 20:05:56 +0000

    Add UK Map

--- src/components/app.js
+++ src/components/app.js
@@ -2,20 +2,12 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import Home from './home'
-import About from './about'
-import PageNotFound from './page-not-found'
+import Map from './Map'
 
 class app extends Component {
   render () {
     return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route path="*" component={PageNotFound} />
-        </Switch>
-      </Router>
+      <Map/>
     )
   }
 }
