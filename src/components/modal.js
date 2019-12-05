e8592f56d0365aa59b5c95033a5016b8730902e7
andypagdin <andyjpagdin@gmail.com>
2019-12-05 21:50:11 +0000

    modal styling

--- src/components/modal.js
+++ src/components/modal.js
@@ -7,34 +7,48 @@ export default function home () {
   return (
     <Modal.Dialog className="region-modal">
       <Modal.Header closeButton>
-        <Modal.Title>Selected region - breakdown</Modal.Title>
+        <Modal.Title>London</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         {/* percentage change modal content */}
-
         {/* Top 5 shows in this region */}
-        <Row>
-          <Col>
-            <ListGroup className="top-shows">
-              <ListGroup.Item>1. RuPauls Drag Race UK</ListGroup.Item>
-              <ListGroup.Item>2. Hometown: A Killing</ListGroup.Item>
-              <ListGroup.Item>3. The Young Offenders</ListGroup.Item>
-              <ListGroup.Item>4. Tagged: Life On Lockdown</ListGroup.Item>
-              <ListGroup.Item>5. Ladhood</ListGroup.Item>
-            </ListGroup>
-          </Col>
-          {/* <Col sm={4}>
-            <img src={DemographicImage} width="200" height="200" />
-          </Col> */}
-        </Row>
+        <div className={"row-wrapper"}>
+          <Row><h4>Top 5 Programmes</h4></Row>
+          <Row className={"top-shows-row"}>
+            <Col>
+              <ListGroup className="top-shows">
+                <ListGroup.Item>1. RuPauls Drag Race UK</ListGroup.Item>
+                <ListGroup.Item>2. Hometown: A Killing</ListGroup.Item>
+                <ListGroup.Item>3. The Young Offenders</ListGroup.Item>
+                <ListGroup.Item>4. Tagged: Life On Lockdown</ListGroup.Item>
+                <ListGroup.Item>5. Ladhood</ListGroup.Item>
+              </ListGroup>
+            </Col>
+          </Row>
+        </div>
 
-        <Row>
-          <Col>
-            <img src={DemographicImage} width="200" height="200" />
-          </Col>
-        </Row>
+        <div className={"row-wrapper"}>
+          <Row><h4>Demographic Graphics</h4></Row>
+          <Row className={"demographics-row"}>
+            <Col>
+              <img src={DemographicImage} width="150" height="150" />
+            </Col>
+          </Row>
+        </div>
+
         {/* common characteristics of the top 5 shows */}
-        
+        <div className={"row-wrapper"}>
+          <Row><h4>Common Characteristics</h4></Row>
+          <Row className={"common-characteristics-row"}>
+            <Col>
+              <ListGroup className="common-characteristics">
+                <ListGroup.Item>Comedy</ListGroup.Item>
+                <ListGroup.Item>Female lead</ListGroup.Item>
+                <ListGroup.Item>30 minute run time</ListGroup.Item>
+              </ListGroup>
+            </Col>
+          </Row>
+        </div>
 
         {/* google trends? twitter api? scrolling text? trending in this region? */}
 
