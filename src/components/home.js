bd58203dd073aa90e6eac87fad65fa9e049f7893
andypagdin <andyjpagdin@gmail.com>
2019-12-05 22:32:04 +0000

    home styles

--- src/components/home.js
+++ src/components/home.js
@@ -1,19 +1,19 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Row, Col, Container, Navbar } from 'react-bootstrap'
 import  Map from './Map'
 
 export default function home () {
   return (
-  <Container>
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">Threegions</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    </Navbar>
-    <Row>
-      <Col>
-        <Map/>
-      </Col>
-    </Row>
-  </Container>
+    <Fragment>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Threegions</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      </Navbar>
+      <Row>
+        <Col>
+          <Map/>
+        </Col>
+      </Row>
+    </Fragment>
   )
 }
