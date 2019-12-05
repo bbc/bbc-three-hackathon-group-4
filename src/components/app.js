4a6c1c0ea4e29c6ca9804c24ebdd89d01ab57a72
Waddah Mortada <w.mortada87@gmail.com>
2019-12-05 20:17:42 +0000

    Move map out of app

--- src/components/app.js
+++ src/components/app.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import Map from './Map'
-
 class app extends Component {
   render () {
     return (
-      <Map/>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Router>
     )
   }
 }
