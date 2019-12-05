0af74644493e0b8422ab9bd5afcb86eeb3c3c2d8
Madusha <madusha.pallawela@bbc.co.uk>
2019-12-05 22:19:13 +0000

    Update Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -33,9 +33,36 @@ class Map extends Component {
     chart.geodataSource.events.on("parseended", function (ev) {
       let data = [];
       for (var i = 0; i < ev.target.data.features.length; i++) {
+
+
+        var regions = {};
+
+        regions["South East England"] = 11;
+        regions["South West England"] = 0;
+        regions["Greater London"] = 0;
+        regions["East of England"] = 0;
+        regions["East Midlands"] = 0;
+        regions["West Midlands"] = 0;
+        regions["Wales"] = 0;
+        regions["North West Engalnd"] = 0;
+        regions["Yorkshire and the Humber"] = 0;
+        regions["North West England"] = 0;
+        regions["Scotland"] = 0;
+        regions["Northern Ireland"] = 0;
+        regions["Ireland"] = 0;
+
+        let value = 0;
+
+        if(ev.target.data.features[i].properties.name == "South East England"){
+          value = 11;  
+        } 
+
+        console.log(ev.target.data.features[i].properties.name)
+
+
         data.push({
           id: ev.target.data.features[i].id,
-          value: Math.round(Math.random() * 10000)
+          value: value
         })
       }
       polygonSeries.data = data;
@@ -94,6 +121,32 @@ class Map extends Component {
       return "";
     });
 
+
+  // Set up heat legend
+  let heatLegend = chart.createChild(am4maps.HeatLegend);
+  heatLegend.series = polygonSeries;
+  heatLegend.align = "right";
+  heatLegend.width = am4core.percent(25);
+  heatLegend.marginRight = am4core.percent(4);
+  heatLegend.minValue = 0;
+  heatLegend.maxValue = 40000000;
+  heatLegend.valign = "bottom";
+
+  // Set up custom heat map legend labels using axis ranges
+  var minRange = heatLegend.valueAxis.axisRanges.create();
+  minRange.value = heatLegend.minValue;
+  minRange.label.text = "Least Viewed";
+  var maxRange = heatLegend.valueAxis.axisRanges.create();
+  maxRange.value = heatLegend.maxValue;
+  maxRange.label.text = "Most Viewed";
+
+  // Blank out internal heat legend value axis labels
+  heatLegend.valueAxis.renderer.labels.template.adapter.add("text", function(labelText) {
+    return "";
+  });
+
+
+  
     this.chart = chart;
   }
 
